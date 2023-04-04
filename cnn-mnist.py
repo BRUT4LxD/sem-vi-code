9822da83ba946484e8a6a2a2eb4b2e3816d8a2cd
BRUT4LxD <tarsala95@gmail.com>
2023-04-04 22:56:54 +0200

    Add pretratined model fetching

--- cnn-mnist.py
+++ cnn-mnist.py
@@ -15,6 +15,7 @@ num_epochs = 5
 learning_rate = 0.001
 image_size = 28
 MODEL_SAVE_PATH = './models/cnn-mnist.pth'
+LOAD_PRETRAINED = False
 
 transform = transforms.Compose(
   [
@@ -52,60 +53,86 @@ class ConvNet(nn.Module):
     x = F.relu(self.fc2(x))
     x = self.fc3(x)
     return x
-  
-model = ConvNet().to(device)
+
+model = torch.load(MODEL_SAVE_PATH) if LOAD_PRETRAINED else ConvNet().to(device)
 
 criterion = nn.CrossEntropyLoss()
 optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
 
-n_total_steps = len(train_loader)
-for epoch in range(num_epochs):
-  for i, (images, labels) in enumerate(train_loader):
-    images = images.to(device)
-    labels = labels.to(device)
-    
-    outputs = model(images)
-    
-    loss = criterion(outputs, labels)
-    
-    optimizer.zero_grad()
-    loss.backward()
-    optimizer.step()
-    
-    if (i + 1) % 2000 == 0:
-      print(f'epoch {epoch + 1}/{num_epochs}, step {i + 1}/{n_total_steps}, loss = {loss.item():.4f}')
-    
-print('Finished training')
+def train(model, criterion, optimizer, train_loader):
+  n_total_steps = len(train_loader)
+  for epoch in range(num_epochs):
+    for i, (images, labels) in enumerate(train_loader):
+      images = images.to(device)
+      labels = labels.to(device)
+      
+      outputs = model(images)
+      
+      loss = criterion(outputs, labels)
+      
+      optimizer.zero_grad()
+      loss.backward()
+      optimizer.step()
+      
+      if (i + 1) % 2000 == 0:
+        print(f'epoch {epoch + 1}/{num_epochs}, step {i + 1}/{n_total_steps}, loss = {loss.item():.4f}')
+      
+  print('Finished training')
 
-torch.save(model, MODEL_SAVE_PATH)
+def save(model):
+  torch.save(model, MODEL_SAVE_PATH)
 
-with torch.no_grad():
-  n_correct = 0
-  n_samples = 0 
-  n_class_correct = [0 for i in range(10)]
-  n_class_samples = [0 for i in range(10)]
-  for images, labels in test_loader: 
-    images = images.to(device)
-    labels = labels.to(device)
-    outputs = model(images)
+def validation(model, test_loader):
+  with torch.no_grad():
+    n_correct = 0
+    n_samples = 0 
+    n_class_correct = [0 for i in range(10)]
+    n_class_samples = [0 for i in range(10)]
+    for images, labels in test_loader: 
+      images = images.to(device)
+      labels = labels.to(device)
+      outputs = model(images)
+      
+      _, predictions = torch.max(outputs, 1)
+      n_samples += labels.size(0)
+      n_correct += (predictions == labels).sum().item()
+      
+      for i in range(batch_size):
+        label = labels[i]
+        pred = predictions[i]
+        if (label == pred):
+          n_class_correct[label] += 1
+        n_class_samples[label] += 1     
+      
+    acc = 100.0 * n_correct / n_samples
+    print(f'accuracy = {acc}%')
     
-    _, predictions = torch.max(outputs, 1)
-    n_samples += labels.size(0)
-    n_correct += (predictions == labels).sum().item()
+    for i in range(10):
+      acc = 100.0 * n_class_correct[i] / n_class_samples[i]
+      print(f'accuracy of {mnist_classes[i]}: {acc}%')
     
-    for i in range(batch_size):
-      label = labels[i]
-      pred = predictions[i]
-      if (label == pred):
-        n_class_correct[label] += 1
-      n_class_samples[label] += 1     
+def visualize(test_model):
+  batch_cnt = 0
+  with torch.no_grad():
+    for images, labels in test_loader:
+      if batch_cnt > 5:
+        break
+      batch_cnt += 1
+      images = images.to(device)
+      labels = labels.to(device)
+      outputs = test_model(images)
+      _, predictions = torch.max(outputs, 1)
+      print('predictions: ', ' '.join('%5s' % mnist_classes[predictions[j]] for j in range(batch_size)))
+      
+      images = images.cpu().numpy()
+      images = np.transpose(images, (0, 2, 3, 1))
+      for i in range(batch_size):
+        plt.figure()
+        plt.imshow(images[i])
+        plt.title(f'predicted: {mnist_classes[predictions[i]]}, label: {mnist_classes[labels[i]]}')
+        plt.show()
     
-  acc = 100.0 * n_correct / n_samples
-  print(f'accuracy = {acc}%')
-  
-  for i in range(10):
-    acc = 100.0 * n_class_correct[i] / n_class_samples[i]
-    print(f'accuracy of {mnist_classes[i]}: {acc}%')
-  
-  
-    
+saved_model = torch.load(MODEL_SAVE_PATH)
+   
+visualize(saved_model)
+
