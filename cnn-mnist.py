02fdb666aea1271b9d6fab01933e6e51a9f35d8e
BRUT4LxD <tarsala95@gmail.com>
2023-04-04 19:47:09 +0200

    Add mnist model

--- cnn-mnist.py
+++ cnn-mnist.py
@@ -8,35 +8,38 @@ import numpy as np
 
 # device config
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+print(f'Using device: {device}')
 
 batch_size = 4
 num_epochs = 5
 learning_rate = 0.001
+image_size = 28
+MODEL_SAVE_PATH = './models/cnn-mnist.pth'
 
 transform = transforms.Compose(
   [
     transforms.ToTensor(),
-    transforms.Normalize((0.5,0.5,0.5), (0.5,0.5,0.5))
+    transforms.Normalize((0.5), (0.5))
   ]
 )
 
-train_dataset = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform)
-test_dataset = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=transform)
+train_dataset = torchvision.datasets.MNIST(root='./data', train=True, download=True, transform=transform)
+test_dataset = torchvision.datasets.MNIST(root='./data', train=False, download=True, transform=transform)
 
 train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
 test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
 
 # cifar10 classes
-classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+mnist_classes = ('0', '1', '2', '3', '4', '5', '6', '7', '8', '9'); 
 
 # implement conv net 
 class ConvNet(nn.Module):
   def __init__(self):
     super(ConvNet, self).__init__()
-    self.conv1 = nn.Conv2d(3, 6, 5)
+    self.conv1 = nn.Conv2d(1, 6, 5)
     self.pool = nn.MaxPool2d(2,2)
     self.conv2 = nn.Conv2d(6, 16, 5)
-    self.fc1 = nn.Linear(16*5*5, 120)
+    self.fc1 = nn.Linear(16*4*4, 120)
     self.fc2 = nn.Linear(120, 84)
     self.fc3 = nn.Linear(84, 10)
     
@@ -44,7 +47,7 @@ class ConvNet(nn.Module):
   def forward(self, x):
     x = self.pool(F.relu(self.conv1(x)))
     x = self.pool(F.relu(self.conv2(x)))
-    x = x.view(-1, 16*5*5)
+    x = x.view(-1, 16*4*4)
     x = F.relu(self.fc1(x))
     x = F.relu(self.fc2(x))
     x = self.fc3(x)
@@ -74,6 +77,8 @@ for epoch in range(num_epochs):
     
 print('Finished training')
 
+torch.save(model, MODEL_SAVE_PATH)
+
 with torch.no_grad():
   n_correct = 0
   n_samples = 0 
@@ -100,7 +105,7 @@ with torch.no_grad():
   
   for i in range(10):
     acc = 100.0 * n_class_correct[i] / n_class_samples[i]
-    print(f'accuracy of {classes[i]}: {acc}%')
+    print(f'accuracy of {mnist_classes[i]}: {acc}%')
   
   
     
