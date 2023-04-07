e882dd813eacdea0973e29321fc3dc53c4a0e5e4
BRUT4LxD <tarsala95@gmail.com>
2023-04-07 02:44:20 +0200

    Add models for classification

--- adversarial-examples/fgsm-mnist.py
+++ adversarial-examples/fgsm-mnist.py
@@ -13,15 +13,20 @@ model = torch.load(MNIST_PRETRAINED_PATH)
 batch_size = 1
 epsilons = [0, .05, .1, .15, .2, .25, .3]
 
-transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5,), (0.5,))])
-test_dataset = torchvision.datasets.MNIST(root='./data', train=False, download=True, transform=transform)
-test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=batch_size, shuffle=True)
+transform = transforms.Compose(
+    [transforms.ToTensor(), transforms.Normalize((0.5,), (0.5,))])
+test_dataset = torchvision.datasets.MNIST(
+    root='./data', train=False, download=True, transform=transform)
+test_loader = torch.utils.data.DataLoader(
+    test_dataset, batch_size=batch_size, shuffle=True)
+
 
 def fgsm_attack(image, epsilon, data_grad):
     sign_data_grad = data_grad.sign()
     perturbed_image = image + epsilon * sign_data_grad
     perturbed_image = torch.clamp(perturbed_image, 0, 1)
     return perturbed_image
+
 
 def evaluate(model, test_loader):
     correct = 0
@@ -35,10 +40,11 @@ def evaluate(model, test_loader):
     accuracy = 100 * correct / total
     print('Accuracy on original test set: {:.2f}%'.format(accuracy))
 
+
 def attack(model, test_loader, epsilon):
     """
     Generates adversarial examples using the FGSM attack and evaluates the model on them.
-    
+
     Parameters:
     model (nn.Module): the pre-trained PyTorch model to be attacked
     test_loader (DataLoader): the data loader for the test set
@@ -49,30 +55,30 @@ def attack(model, test_loader, epsilon):
     adv_examples = []
     loader_cnt = 0
     criterion = nn.CrossEntropyLoss()
-    
+
     print(f'Running attack for epsilon: {epsilon}')
     for images, labels in test_loader:
-        if loader_cnt % 1000 == 0: 
+        if loader_cnt % 1000 == 0:
             print(f'attacked: {loader_cnt}')
         loader_cnt += 1
-        
+
         images, labels = images.to(device), labels.to(device)
         images.requires_grad = True
         init_pred = model(images)
         _, init_predicted = torch.max(init_pred, 1)
-        
+
         if(init_predicted != labels):
             continue
-        
+
         loss = criterion(init_pred, labels)
         model.zero_grad()
         loss.backward()
-        
+
         data_grad = images.grad
         perturbed_images = fgsm_attack(images, epsilon, data_grad)
         outputs = model(perturbed_images)
         _, predicted = torch.max(outputs, 1)
-        
+
         match_vector = predicted == labels
         total += labels.size(0)
         correct += match_vector.sum().item()
@@ -80,13 +86,14 @@ def attack(model, test_loader, epsilon):
             if match_vector[i] == False and len(adv_examples) < 5:
                 adv_ex = perturbed_images[i].squeeze().detach().cpu().numpy()
                 adv_examples.append((init_predicted[i], predicted[i], adv_ex))
-        
-    
+
     # Compute the accuracy of the model on the adversarial test set
     accuracy = 100 * correct / total
-    print('Epsilon: {:.4f}\tAccuracy on adversarial test set: {:.2f}%'.format(epsilon, accuracy))
-    
+    print('Epsilon: {:.4f}\tAccuracy on adversarial test set: {:.2f}%'.format(
+        epsilon, accuracy))
+
     return adv_examples, accuracy
+
 
 def run_attack(model, test_loader):
     accuracies = []
@@ -97,8 +104,9 @@ def run_attack(model, test_loader):
         examples.append(adv_ex)
     return examples, accuracies
 
+
 def plot_accuracy(accuracies):
-    plt.figure(figsize=(5,5))
+    plt.figure(figsize=(5, 5))
     plt.plot(epsilons, accuracies, "*-")
     plt.yticks(np.arange(0, 100, step=10))
     plt.xticks(np.arange(0, .35, step=.05))
@@ -108,29 +116,32 @@ def plot_accuracy(accuracies):
     plt.show()
 
 # Plot several examples of adversarial samples at each epsilon
+
+
 def plot_adversarial_examples(examples, epsilons):
     cnt = 0
-    plt.figure(figsize=(7,10))
+    plt.figure(figsize=(7, 10))
     for i in range(len(epsilons)):
         for j in range(len(examples[i])):
             cnt += 1
-            plt.subplot(len(epsilons),len(examples[0]),cnt)
+            plt.subplot(len(epsilons), len(examples[0]), cnt)
             plt.xticks([], [])
             plt.yticks([], [])
             if j == 0:
                 plt.ylabel("Eps: {}".format(epsilons[i]), fontsize=14)
-            orig,adv,ex = examples[i][j]
+            orig, adv, ex = examples[i][j]
             plt.title("{} -> {}".format(orig, adv))
             plt.imshow(ex, cmap="gray")
     plt.tight_layout()
     plt.show()
-    
-model = torch.load(MNIST_PRETRAINED_PATH)    
+
+
+model = torch.load(MNIST_PRETRAINED_PATH)
 
 # attack(model, test_loader, 0.3)
 
 adversarial_examples, accuracies = run_attack(model, test_loader)
-  
+
 plot_accuracy(accuracies)
 
 plot_adversarial_examples(adversarial_examples, epsilons)
--- cnn-cifar.py
+++ cnn-cifar.py
@@ -14,42 +14,49 @@ num_epochs = 5
 learning_rate = 0.001
 
 transform = transforms.Compose(
-  [
-    transforms.ToTensor(),
-    transforms.Normalize((0.5,0.5,0.5), (0.5,0.5,0.5))
-  ]
+    [
+        transforms.ToTensor(),
+        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+    ]
 )
 
-train_dataset = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform)
-test_dataset = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=transform)
+train_dataset = torchvision.datasets.CIFAR10(
+    root='./data', train=True, download=True, transform=transform)
+test_dataset = torchvision.datasets.CIFAR10(
+    root='./data', train=False, download=True, transform=transform)
 
-train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
-test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
+train_loader = torch.utils.data.DataLoader(
+    train_dataset, batch_size=batch_size, shuffle=True)
+test_loader = torch.utils.data.DataLoader(
+    test_dataset, batch_size=batch_size, shuffle=False)
 
 # cifar10 classes
-classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+classes = ('plane', 'car', 'bird', 'cat', 'deer',
+           'dog', 'frog', 'horse', 'ship', 'truck')
 
-# implement conv net 
+# implement conv net
+
+
 class ConvNet(nn.Module):
-  def __init__(self):
-    super(ConvNet, self).__init__()
-    self.conv1 = nn.Conv2d(3, 6, 5)
-    self.pool = nn.MaxPool2d(2,2)
-    self.conv2 = nn.Conv2d(6, 16, 5)
-    self.fc1 = nn.Linear(16*5*5, 120)
-    self.fc2 = nn.Linear(120, 84)
-    self.fc3 = nn.Linear(84, 10)
-    
-  
-  def forward(self, x):
-    x = self.pool(F.relu(self.conv1(x)))
-    x = self.pool(F.relu(self.conv2(x)))
-    x = x.view(-1, 16*5*5)
-    x = F.relu(self.fc1(x))
-    x = F.relu(self.fc2(x))
-    x = self.fc3(x)
-    return x
-  
+    def __init__(self):
+        super(ConvNet, self).__init__()
+        self.conv1 = nn.Conv2d(3, 6, 5)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        self.fc1 = nn.Linear(16*5*5, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
+
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = x.view(-1, 16*5*5)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+
+
 model = ConvNet().to(device)
 
 criterion = nn.CrossEntropyLoss()
@@ -57,50 +64,48 @@ optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
 
 n_total_steps = len(train_loader)
 for epoch in range(num_epochs):
-  for i, (images, labels) in enumerate(train_loader):
-    images = images.to(device)
-    labels = labels.to(device)
-    
-    outputs = model(images)
-    
-    loss = criterion(outputs, labels)
-    
-    optimizer.zero_grad()
-    loss.backward()
-    optimizer.step()
-    
-    if (i + 1) % 2000 == 0:
-      print(f'epoch {epoch + 1}/{num_epochs}, step {i + 1}/{n_total_steps}, loss = {loss.item():.4f}')
-    
+    for i, (images, labels) in enumerate(train_loader):
+        images = images.to(device)
+        labels = labels.to(device)
+
+        outputs = model(images)
+
+        loss = criterion(outputs, labels)
+
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        if (i + 1) % 2000 == 0:
+            print(
+                f'epoch {epoch + 1}/{num_epochs}, step {i + 1}/{n_total_steps}, loss = {loss.item():.4f}')
+
 print('Finished training')
 
 with torch.no_grad():
-  n_correct = 0
-  n_samples = 0 
-  n_class_correct = [0 for i in range(10)]
-  n_class_samples = [0 for i in range(10)]
-  for images, labels in test_loader: 
-    images = images.to(device)
-    labels = labels.to(device)
-    outputs = model(images)
-    
-    _, predictions = torch.max(outputs, 1)
-    n_samples += labels.size(0)
-    n_correct += (predictions == labels).sum().item()
-    
-    for i in range(batch_size):
-      label = labels[i]
-      pred = predictions[i]
-      if (label == pred):
-        n_class_correct[label] += 1
-      n_class_samples[label] += 1     
-    
-  acc = 100.0 * n_correct / n_samples
-  print(f'accuracy = {acc}%')
-  
-  for i in range(10):
-    acc = 100.0 * n_class_correct[i] / n_class_samples[i]
-    print(f'accuracy of {classes[i]}: {acc}%')
-  
-  
-    
+    n_correct = 0
+    n_samples = 0
+    n_class_correct = [0 for i in range(10)]
+    n_class_samples = [0 for i in range(10)]
+    for images, labels in test_loader:
+        images = images.to(device)
+        labels = labels.to(device)
+        outputs = model(images)
+
+        _, predictions = torch.max(outputs, 1)
+        n_samples += labels.size(0)
+        n_correct += (predictions == labels).sum().item()
+
+        for i in range(batch_size):
+            label = labels[i]
+            pred = predictions[i]
+            if (label == pred):
+                n_class_correct[label] += 1
+            n_class_samples[label] += 1
+
+    acc = 100.0 * n_correct / n_samples
+    print(f'accuracy = {acc}%')
+
+    for i in range(10):
+        acc = 100.0 * n_class_correct[i] / n_class_samples[i]
+        print(f'accuracy of {classes[i]}: {acc}%')
