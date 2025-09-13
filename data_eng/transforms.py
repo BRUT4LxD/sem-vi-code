119f28c6f388642c8072b523a2ae86a5cff2e625
BRUT4LxD <tarsala95@gmail.com>
2025-09-13 17:52:16 +0200

    Add normalization

--- data_eng/transforms.py
+++ data_eng/transforms.py
@@ -34,3 +34,14 @@ def imagenette_transformer():
             #                      std=[0.229, 0.224, 0.225])
         ]
     )
+    
+def imagenet_transformer():
+    return transforms.Compose(
+        [
+            transforms.Resize(256),
+            transforms.CenterCrop(224),
+            transforms.ToTensor(),
+            transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                 std=[0.229, 0.224, 0.225])
+        ]
+    )
